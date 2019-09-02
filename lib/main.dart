ab3257d50a3028b31c9b5e5c4c530c658c9976d4
Hang Chen <hangisnice@gmail.com>
2019-09-02 12:29:21 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'comsumer_demo.dart';
+import 'provider_demo.dart';
+import 'counter_model.dart';
+
+void main() {
+
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      Provider.value(value: 30.0),
+      ChangeNotifierProvider.value(value: CounterModel())
+    ],child: MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage()
+    ));
+
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: TabBarView(
+          children: [
+            ConsumerTabPage1(),
+            ProviderTabPage1(),
+          ],
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.home),text: "Consumer",),
+            Tab(icon: Icon(Icons.rss_feed),text: "Provider",),
+          ],
+          unselectedLabelColor: Colors.blueGrey,
+          labelColor: Colors.blue,
+          indicatorSize: TabBarIndicatorSize.label,
+          indicatorColor: Colors.red,
+        ),
+      ),
+    );
+  }
+
+}
+
+
